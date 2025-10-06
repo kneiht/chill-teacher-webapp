899e8c8dd2b2f663efdba5cdc728d9c0c22f9797
Thien Nguyen <minhthien.k@gmail.com>
2025-10-06 15:05:09 +0700

    feat(lessons): calculate height for GoogleSlide

--- src/lib/components/presentation/GoogleSlide.tsx
+++ src/lib/components/presentation/GoogleSlide.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react'
+import React from 'react'
 
 interface GoogleSlideProps {
   isActive: boolean
@@ -7,33 +7,6 @@ interface GoogleSlideProps {
 }
 
 const GoogleSlide: React.FC<GoogleSlideProps> = ({ isActive, src, style }) => {
-  const [iframeHeight, setIframeHeight] = useState('103.9%')
-
-  useEffect(() => {
-    const calculateHeight = () => {
-      const screenWidth = window.innerWidth
-      const minWidth = 340 // Breakpoint for small screens
-      const maxWidth = 1920 // Breakpoint for large screens
-      const maxHeight = 120 // Height for small screens
-      const minHeight = 103.9 // Height for large screens
-
-      // Linear interpolation between the two points
-      const slope = (screenWidth - minWidth) / (maxWidth - minWidth)
-      const height = minHeight + slope * (screenWidth - minWidth)
-
-      setIframeHeight(`${height}%`)
-      console.log(height)
-    }
-
-    calculateHeight() // Calculate initial height
-
-    window.addEventListener('resize', calculateHeight)
-
-    return () => {
-      window.removeEventListener('resize', calculateHeight)
-    }
-  }, [])
-
   return (
     <div
       className={`slide rounded-xl w-full h-full ${isActive ? 'active' : ''}`}
@@ -41,9 +14,7 @@ const GoogleSlide: React.FC<GoogleSlideProps> = ({ isActive, src, style }) => {
     >
       <iframe
         src={src}
-        style={{ border: 0 }}
-        width="100%"
-        height={iframeHeight}
+        style={{ border: 0, width: '100%', height: 'calc(100% + 36px)' }}
         allowFullScreen
         className="rounded-xl"
       ></iframe>
